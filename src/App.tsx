19b1e88c00d47e62bccc26a512c482ba2b167ca9
JefersonLRS <contato.jefersonlrs@gmail.com>
2024-04-17 19:52:39 -0300

    fix: Responsive Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
 import Introduction from './components/Introduction'
+import cadeadoIcon from './assets/images/cadeadoIcone.svg'
+import estrategiaIcon from './assets/images/estrategiaIcone.svg'
+// import faixaIcon from './assets/images/faixaIcone.svg'
+// import hackIcon from './assets/images/hackIcone.svg'
 
 function App() {
   return (
-    <div className='m-auto flex justify-center w-full h-screen'>
+    <div className='flex flex-col justify-center'>
       <Introduction/>
     </div>
   )
--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,12 +4,12 @@ import imagemHeader from '../assets/images/imagemHeader.png'
 
 export default function Introduction () {
     return (
-      <div>
+      <div className='w-[1300px] mx-auto'>
         <div className='absolute flex flex-col gap-12 text-white p-16'>
-        <div className='text-4xl font-black'>
-          <h1>AS INOVAÇÕES E AVANÇOS MAIS</h1>
-          <h1>RECENTES DO <span className='text-[#FDE936]'>MARKETING DIGITAL</span></h1>
-        </div>
+          <div className='text-4xl font-black'>
+            <h1>AS INOVAÇÕES E AVANÇOS MAIS</h1>
+            <h1>RECENTES DO <span className='text-[#FDE936]'>MARKETING DIGITAL</span></h1>
+          </div>
         <div className='font-medium text-3xl w-[600px]'>
           <p>Que estão gerando <span className='text-[#FDE936]'>milhões em vendas</span> na internet, totalmente ao seu alcance para replicar</p>
         </div>
@@ -22,7 +22,7 @@ export default function Introduction () {
         </div>
 
         <form className='flex flex-col w-[580px]'>
-          <label className='mb-3 text-sm'>Deixe seu nome e e-mail abaixo para acessar o curso e o certificado <span className='font-bold'>gratuitamente:</span></label>
+          <label className='mb-3 text-[15px]'>Deixe seu nome e e-mail abaixo para acessar o curso e o certificado <span className='font-bold text-lg'>gratuitamente:</span></label>
           <input placeholder='Digite seu email...' className='h-16 p-6 font-medium text-lg text-black' type="text"/>
           <button className='bg-[#fde936] text-black p-6 font-black mt-3'>QUERO PARTICIPAR AO VIVO</button>
           <img src={faixaPretaButton} alt="faixa preta" />
@@ -30,8 +30,8 @@ export default function Introduction () {
 
       </div>
       <div >
-        <img className='m-auto' src={imagemHeader}/>
+        <img className='mx-auto' src={imagemHeader}/>
       </div>
-    </div>
+      </div>
     )
 }
